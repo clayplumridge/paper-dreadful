f777579af148b2d3bbb76f1abd99fb3647d14eab
Clay Plumridge <plumridgec@gmail.com>
2023-11-01 18:38:24 -0400

    Set up non-relative path imports

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
+import { SOME_NUMBER } from "@/common/test";
 
 const App: React.FC<unknown> = () => {
-    return <div>Hello World!</div>;
+    return <div>Hello World! {SOME_NUMBER}</div>;
 };
 
 const container = document.getElementById("react-root")!;
